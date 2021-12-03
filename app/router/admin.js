99cc458eb8793189aa24132c187b943bf51209bd
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-02 19:31:38 -0600

    write a register post in admin route

--- app/router/admin.js
+++ app/router/admin.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import adminController from "../controllers/admin.js";
 
 const router = new Router();
 router.get("/", (req, res) => {
   res.send("Admin");
 });
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    adminController.create(username, password);
+    res.send({ message: "Register" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 export default router;
