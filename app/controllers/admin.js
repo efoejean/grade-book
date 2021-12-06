2fc4896d93a3f76a1a00c2c5e5e377bba965ef6b
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-05 21:24:25 -0600

    Encrpyt the password

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,14 +1,20 @@
 import config from "../config.js";
 import client from "../client.js";
+import bcrypt from "bcrypt";
 
 const admin = client.db(config.db.name).collection("admin");
 export default {
   async create(username, password) {
     // Check for an existing user in the db
     const existingUser = await admin.findOne({ username });
+
     if (existingUser) {
       throw new Error("User already exists");
     }
+
+    // encrypt or Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     console.log(username, password, "from admin controller");
   },
 };
