d31dc7a9f7706abc1c83f1d5ba13a53a8c6fc340
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-05 22:28:31 -0600

    import client, bcrypt, jwt and create a login method.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,6 +1,7 @@
-import config from "../config.js";
-import client from "../client.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../client.js";
+import config from "../config.js";
 
 const admin = client.db(config.db.name).collection("admin");
 export default {
@@ -12,9 +13,33 @@ export default {
       throw new Error("User already exists");
     }
 
+    // if not,
     // encrypt or Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    console.log(username, password, "from admin controller");
+    return admin.insertOne({ username, password: hashedPassword });
+  },
+
+  // login method
+  async login(username, password) {
+    // find the user by username
+    const user = await admin.findOne({ username });
+
+    if (!user) {
+      throw new Error("Unable to login");
+    }
+
+    // check if the password is correct
+    // compare the password with the hashed password
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      throw new Error("Unable to login");
+    }
+
+    // generate and return a JWT to be used for future requests by the user
+    return jwt.sign({ username }, config.encryption.secret, {
+      expiresIn: config.encryption.expiresIn,
+    });
   },
 };
