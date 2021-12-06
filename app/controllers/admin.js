03a76dcb81ca7cfd56f30d4837790a84b83edb3b
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-05 21:13:50 -0600

    Check for an existing user in the db

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,8 +3,12 @@ import client from "../client.js";
 
 const admin = client.db(config.db.name).collection("admin");
 export default {
-  create(username, password) {
+  async create(username, password) {
     // Check for an existing user in the db
+    const existingUser = await admin.findOne({ username });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
     console.log(username, password, "from admin controller");
   },
 };
