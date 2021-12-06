fbe166d0bfa99da8a0c523e47805e1448dceb3c7
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-05 22:26:37 -0600

    Add encryption configuration to the config

--- app/config.js
+++ app/config.js
@@ -6,8 +6,13 @@ export default {
   baseURL: process.env.BASE_URL || "http://localhost:3000",
   db: {
     URL: process.env.MONGO_DB_URL,
-    name: "gradeBooksDB",
-    collection: "students",
+    name: "Gradebook",
+  },
+
+  encryption: {
+    expiresIn: process.env.EXPIRES_IN || "2d",
+    saltRounds: process.env.SALT_ROUNDS || 10,
+    secret: process.env.ENCRYPTION_SECRET || "secret",
   },
 
   port: process.env.PORT || 3000,
