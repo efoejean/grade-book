87f98a7531e3d9b9dd1788a14bec30a3f5c17a08
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-05 22:29:29 -0600

    // Use the login method in the adminController to login the user

--- app/router/admin.js
+++ app/router/admin.js
@@ -8,9 +8,13 @@ router.get("/", (req, res) => {
 
 router.post("/register", async (req, res) => {
   try {
+    // destructure the username and password from  req.body
     const { username, password } = req.body;
-    adminController.create(username, password);
-    res.send({ message: "Register" });
+    await adminController.create(username, password);
+
+    // Use the login method in the adminController to login the user
+    const token = await adminController.login(username, password);
+    res.send(token);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
