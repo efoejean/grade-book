d3c1504c407c61148d51bf58dafbe9c4fb4f26d3
efoejean <77749280+efoejean@users.noreply.github.com>
2021-12-12 00:17:03 -0600

    / get one student using a dynamic route using student id, and destructure the req.

--- app/router/student.js
+++ app/router/student.js
@@ -20,15 +20,32 @@ router.post("/", async (req, res) => {
   }
 });
 
-// dynamic route using student id and body
-router.post("/grade/:id", async (req, res) => {
-  if (req.isAuth?.role === "ADMIN") {
+// get one student using a dynamic route using student id, and destructure the req. we use post because we are send token for verification.
+router.post("/:id", async ({ isAuth, params }, res) => {
+  if (isAuth.role === "ADMIN") {
     try {
-      const update = await stuController.update(req.params.id, req.body);
+      const update = await stuController.show(params.id);
       res.json(update);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
+  } else {
+    res.status(401).json({ message: "Access denied" });
   }
 });
+
+// update students grades,  use a dynamic route using student id and body
+router.put("/grade/:id", async (req, res) => {
+  if (req.isAuth?.role === "ADMIN") {
+    try {
+      const update = await stuController.update(req.params.id, req.body);
+      res.status(200).json(update);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(401).json({ message: "Access denied" });
+  }
+});
+
 export default router;
